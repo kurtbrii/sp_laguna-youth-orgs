6461ab69c3ad3406c9278d3c0f1b8e1914651733
kbpunzalan <kbpunzalan@up.edu.ph>
2024-02-07 20:10:58 +0800

    debug: provider not found login

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import vol2 from "../../../assets/vol2.png";
 import Image from "next/image";
-import Providers from "~/components/Providers";
+// import Providers from "~/components/Providers";
+import { signIn } from "next-auth/react";
+import google from "../../assets/google.png";
+import discord from "../../assets/discord.png";
+import guest from "../../assets/guest.png";
 
 const login = () => {
+  const providers = ["google", "discord"];
+
   return (
     <section className="flex h-screen font-custom-lexend ">
       {/* <!-- Left part with image --> */}
@@ -34,7 +40,58 @@ const login = () => {
         </h1>
 
         {/* providers */}
-        <Providers />
+        {/* <Providers /> */}
+        <div className="flex flex-col  justify-center gap-3 ">
+          {providers.map((provider, index) => (
+            <button
+              type="button"
+              onClick={() => signIn(provider, { callbackUrl: "/homepage" })}
+              key={index}
+              className="btn-outline border-1 flex items-center gap-3 rounded-md border px-10 py-1"
+            >
+              <Image
+                className="h-10 w-10 object-cover"
+                src={provider == "google" ? google : discord}
+                width={20}
+                height={20}
+                alt={`${provider} Image`}
+              />
+              {/* <img
+            src={`https://example.com/${provider.toLowerCase()}-logo.png`}
+            alt={`${provider} Logo`}
+            className="mb-2 h-16 w-16"
+          /> */}
+              <p className="text-md font-semibold">
+                Sign In With {capitalize(provider)}
+              </p>
+              {/* Add other content specific to each provider */}
+            </button>
+          ))}
+          <button
+            type="button"
+            onClick={handleHomePage}
+            className="btn-outline border-1 mb-3 flex items-center gap-3 rounded-md border px-10 py-1"
+          >
+            <Image
+              className="h-10 w-10 object-cover"
+              src={guest}
+              width={30}
+              height={30}
+              alt="Guest Image"
+            />
+            <p className="text-md font-semibold">Continue as Guest</p>
+          </button>
+          <p className="text-center text-sm text-customBlack-100">
+            Don&apos;t have an account yet?{" "}
+            <span
+              onClick={handleSignUp}
+              className="text-gradient cursor-pointer underline-offset-4 "
+            >
+              Sign Up
+            </span>
+            .
+          </p>
+        </div>
         {/* <div>
           <h1 className="mb-3  text-4xl">Welcome Back!</h1>
           <h1 className="mb-3  text-4xl">Welcome Back!</h1>
@@ -43,5 +100,17 @@ const login = () => {
     </section>
   );
 };
+
+function capitalize(word: string) {
+  return word.charAt(0).toUpperCase() + word.toLocaleLowerCase().substring(1);
+}
+
+function handleSignUp() {
+  window.location.href = "/auth/signup";
+}
+
+function handleHomePage() {
+  window.location.href = "/homepage";
+}
 
 export default login;
